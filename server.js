1ab55e628a84c3f93f6f769aabd335c619373049
WIBR <waffeu12092001@mail.com>
2021-08-31 13:15:33 +0200

    test des modules 'socket.io' et express

--- server.js
+++ server.js
@@ -2,12 +2,21 @@ const app = require('express')();
 const server = require('http').Server(app); 
 const io = require('socket.io')(server);
 
+
+// variable
+
+let username = '';
+
 app.get('/',(req, resp)=>{
     resp.sendFile(`${__dirname}/index.html`);
 })
 
 io.on('connection', function (socket){ 
-    console.log('Je suis connecte');
+    // 
+    socket.on('message', (elt)=>{
+        username = elt;
+        socket.emit('receive', username);
+    })
  });
 
 server.listen(3000, ()=>{
